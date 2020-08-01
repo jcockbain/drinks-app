4d31931162455eccc23478e8bef6a97f588c2821
James Cockbain <james.cockbain@ibm.com>
2020-08-01 09:39:33 +0100

    add initial features

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import { format, compareAsc } from 'date-fns';
+import axios from 'axios';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import './App.css';
 
+const drinksIcons = {
+  COFFEES: 'coffee-icon-background.png',
+  COCKTAILS: 'cocktail-icon-background.png',
+  BEERS: 'beer-icon-background.png',
+  MILKSHAKES: 'milkshake-icon-background.png',
+};
+
 function App() {
+  const [data, setData] = useState([]);
+
+  const boxes = data.map((event) => (
+    <Row>
+      <Card key={event.id} className="main-box mx-auto my-3 w-20">
+        <Card.Img fluid variant="top" src={`drinks-icons/${drinksIcons[event.type]}`} alt="drinks-icon" />
+        <Card.Body>
+          <Card.Title>{event.title}</Card.Title>
+        </Card.Body>
+      </Card>
+    </Row>
+  ));
+
+  useEffect(() => {
+    async function fetchData() {
+      const result = await axios(
+        'https://mock-api.drinks.test.siliconrhino.io/events',
+      );
+      setData(result.data);
+    }
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App m-auto">
+      <header className="App-header"><img className="header-image" src="drinks-icons/drinks-text.png" alt="drinks-text" /></header>
+      <Container>
+        <Col md={12}>
+          {boxes}
+        </Col>
+      </Container>
     </div>
   );
 }
