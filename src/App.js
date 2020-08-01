c59104528316f3da27fc78a40c151c3d8d5dd761
James Cockbain <james.cockbain@ibm.com>
2020-08-01 10:22:35 +0100

    create grid of events

--- src/App.js
+++ src/App.js
@@ -1,32 +1,31 @@
 import React, { useEffect, useState } from 'react';
-import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import { format, compareAsc } from 'date-fns';
+import { format } from 'date-fns';
 import axios from 'axios';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import './App.scss';
-
-const drinksIcons = {
-  COFFEES: 'coffee-icon-background.png',
-  COCKTAILS: 'cocktail-icon-background.png',
-  BEERS: 'beer-icon-background.png',
-  MILKSHAKES: 'milkshake-icon-background.png',
-};
+import drinksIcons from './utils/drinksIcons';
 
 function App() {
   const [data, setData] = useState([]);
 
   const boxes = data.map((event) => (
-    <Row>
-      <Card key={event.id} className="main-box mx-auto my-3 w-20">
+    <Col md={4}>
+      <Card key={event.id} className="m-3">
         <Card.Img fluid variant="top" src={`drinks-icons/${drinksIcons[event.type]}`} alt="drinks-icon" />
         <Card.Body>
           <Card.Title>{event.title}</Card.Title>
+          <Card.Subtitle className="text-muted">
+            {format(new Date(event.time), 'MM/dd/yyyy')}
+          </Card.Subtitle>
+          <Card.Text>
+            {event.location.name}
+          </Card.Text>
         </Card.Body>
       </Card>
-    </Row>
+    </Col>
   ));
 
   useEffect(() => {
@@ -42,10 +41,10 @@ function App() {
   return (
     <div className="App m-auto">
       <header className="App-header"><img className="header-image" src="drinks-icons/drinks-text.png" alt="drinks-text" /></header>
-      <Container>
-        <Col md={12}>
+      <Container className="max-auto my-3">
+        <Row>
           {boxes}
-        </Col>
+        </Row>
       </Container>
     </div>
   );
---
+++ src/utils/drinksIcons.js
@@ -0,0 +1,6 @@
+export default {
+  COFFEES: 'coffee-icon-background.png',
+  COCKTAILS: 'cocktail-icon-background.png',
+  BEERS: 'beer-icon-background.png',
+  MILKSHAKES: 'milkshake-icon-background.png',
+};
