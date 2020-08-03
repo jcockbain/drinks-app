d8155abcede97959632b3a9efa58930ed65a7f60
James Cockbain <james.cockbain@ibm.com>
2020-08-03 09:14:22 +0100

    update tests to test for search bar in app

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders search helper text', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Search for an event by its title, or the name of its venue/i);
   expect(linkElement).toBeInTheDocument();
 });
